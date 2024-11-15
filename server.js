df5f64297b0b17b86816b332780c2be1b0f6e2b7
ADAM <shavgoniaziz@gmail.com>
2024-11-16 00:57:30 +0500

    BRR: Build express web server

--- server.js
+++ server.js
@@ -1,30 +1,45 @@
 console.log("Web serverni boshlash");
 const express = require('express');
-const app = express(); //express instance yani bu orqali biz express objectni ishlatamiz
-const http = require('http'); //http bu bizning core modulimiz
+const app = express(); // express instance yani bu orqali biz express objectni ishlatamiz
+const http = require('http'); // http bu bizning core modulimiz
 
-//1 Kirish code
-app.use(express.static("public")); //har qanday browserdan kirib kelayotgan zaproslar uchun public folder ochiq degani yani faqat public folderni clientlarga ochib beryapmiz.
-app.use(express.json()); //bizga kirib kelayotgan object formatdagi datani json formatga otkazib beradi 
-app.use(express.urlencoded({extended: true})); // buni yozmasak html forumdan post qilingan malumotlarni express serverimiz ignore qiladi yani serverga kiritmaydi. 
-//2 : Session code
+// 1 **KIRISH** expressga kirib kelgan codelar yoziladi
+app.use(express.static("public")); // har qanday browserdan kirib kelayotgan zaproslar uchun public folder ochiq degani yani faqat public folderni clientlarga ochib beryapmiz
+app.use(express.json()); // kirib kelyotgan json formatdagi datani obj holatga o'girib beradi, bu bizga kirib kelayotgan object formatdagi datani json formatga otkazib beradi
+app.use(express.urlencoded({extended: true})); // buni yozmasak html formdan post qilingan malumotlarni express serverimiz ignore qiladi yani serverga kiritmaydi; HTML form post shaklida kevotgan requestlar qabul qivolamiza
 
-//3 Views code
-//BSSR back-end-server-side renderring yo'nalishini tanladik
-app.set("views","views");
-app.set("view engine","ejs"); //yani views folderni ichidan o'qiydi. ejs orqali back endni ichida front end yasaymiz
+// 2 **Session CODE** 
+// (Bu yerda Session code yozilishi mumkin)
 
-//4 Routing code
-app.get("/hello", function(req, res) {
-    res.end(`<h1 style="background: red">Hello, world by Adambek</h1>`);
+// 3 **VIEWS CODE** Backenda front end yasaymiz (VIEW yasaymiz)
+// BSSR back-end-server-side renderring yo'nalishini tanladik
+app.set("views", "views"); // yani views folderni ichidan o'qiydi
+app.set("view engine", "ejs"); // view engine ejs ligini ko'rsatib beryabmiz, ejs orqali back endni ichida front end yasaymiz
+
+// 4 **Router'larga** mo'ljallangan
+
+app.get("/hello", function(req, res){
+    res.end(`<h1 style="background: orange">HELLO MIT</h1>`);
 });
 
-app.get("/gift", function(req, res) {
-    res.end(`<h1 style="background: yellow">Siz sovg'alar bo'limidasiz</h1>`);
-}); //shu yozgan html code aynan localhost:/gift da kelib chiqadi yani biz bu orqali address shakklantiryapmiz
+app.get("/gift", function(req, res){
+    res.end(`<h1 style="background: green">GIFT PAGE</h1>`);
+});
 
-const server = http.createServer(app);   
+
+// serverni yaratamiz va unga app ni yuklaymiz
+const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, function () {
-    console.log(`The server is running successfully on ${PORT}`);
+server.listen(PORT, function() {
+    console.log(`The server is running successfully on port: ${PORT}`);
 });
+
+// express orqali web server qurib oldik.
+// front end'ni qurish 2 usuli bor: 1) Backend'da front qurib olish BSSR using EJS 2) React - single page rendering
+
+// git clean -df oxirgi ruchnoy kiritgan narsalarni udalit qiladi
+// git pull origin master orqali eng oxirgi source ni yuklab olamiz
+// git ni bitta orqaga qaytarish uchun
+// git reset --hard desak eng oxirgi deploy qilingan holga qaytaradi kodni
+// get bizga malumotni data base dan olish uchun ishlatilinadi 
+// post esa malumotni ozi bilan olib keladi va data base ga malum bir malumotni yozadi
