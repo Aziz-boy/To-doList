7bbe51343b211c6649379a4b8893dfa0c57e585c
ADAM <shavgoniaziz@gmail.com>
2024-11-21 13:49:09 +0500

    fix:rearranged the code

--- train.js
+++ train.js
@@ -12,40 +12,39 @@ Qilgan Yechimingiz…*/
 
 //1-yechim
 
-function countLetter(e, enginner) {
-   let Ncount = 0
-    for (let i = 0; i < enginner.length; i++) {
-        if (enginner.charAt(i) === e ) {
-            Ncount ++;
-        }
-    }
-    return Ncount;
-}
-
-const countedLetter = countLetter("e", "engineer");
-console.log(countedLetter)
-
-// 2-yechim
-
 // function countLetter(e, enginner) {
-
-// const list = enginner.split("");
-// const count = list.reduce((acc,char,) => {
-//       if (char === e ) {
-//         return acc + 1;
-//       }
-//       return acc;
-// },0);
-
-// return count;
+//    let Ncount = 0
+//     for (let i = 0; i < enginner.length; i++) {
+//         if (enginner.charAt(i) === e ) {
+//             Ncount ++;
+//         }
+//     }
+//     return Ncount;
 // }
 
 // const countedLetter = countLetter("e", "engineer");
 // console.log(countedLetter)
 
+// 2-yechim
+
+function countLetter(e, enginner) {
+const list = enginner.split("");
+const count = list.reduce((acc,char,) => {
+      if (char === e ) {
+        return acc + 1;
+      }
+      return acc;
+},0);
+
+return count;
+}
+
+const countedLetter = countLetter("e", "enginneer");
+console.log(countedLetter)
 
 
-/*           //=====================CALLBACK FUNCTION ==============
+
+/*           //=====================CALLBACK FUNCTION =====================
 const list = [
  "Focus on learning, curiosity, failure, and building good habits.", //Ages 0-20
  "Learn from mistakes, work hard, build a network, and think long-term.", //Ages 20-30
@@ -79,7 +78,7 @@ maslahatBering(10,(err,data) => {
 
 */
 
-/*                     //===================== EVENT LOOP HODISASI ==============
+/*           //===================== EVENT LOOP HODISASI =====================
 const list = [
     "Focus on learning, curiosity, failure, and building good habits.", //Ages 0-20
     "Learn from mistakes, work hard, build a network, and think long-term.", //Ages 20-30
@@ -120,7 +119,7 @@ console.log("train Area ishga tushdi !");
 */
 
 
-                                        //===================== ASYNCHRONOUS FUNCTION==============
+            //===================== ASYNCHRONOUS FUNCTION=========================
 const list = [
     "Focus on learning, curiosity, failure, and building good habits.", //Ages 0-20
     "Learn from mistakes, work hard, build a network, and think long-term.", //Ages 20-30
@@ -129,7 +128,7 @@ const list = [
     "Reflect on achievements, give back to society, and continue learning.", //Ages 50-60
    "Enjoy life, focus on health, and share your wisdom with the next generation." //Ages 60+
    ];                    
-//DEFINITION QISMIDA ASYNCH ISHLATDIK
+            //===================== DEFINITION QISMIDA ASYNCH ISHLATDIK==============
 // async function maslahatBering(a) {
 //     if (typeof a !== 'number') throw new Error ("insert a number"); //agar kiritilgan a argumentni taypi number bolmasa error throw qilsin
 //     else if (a <=20 ) return list[0];
@@ -172,33 +171,33 @@ console.log('passed here 2'); // ikkinchi bu ishladi
 
 
 
-//                             ============Async Await Method ================= CALL VIA ASYNCH AWAIT
+             //==========================CALL VIA Async Await Method ================= 
 
-function maslahatBering(a,callback) {
-    if (typeof a !== 'number') callback("insert a number",null); //agar kiritilgan a argumentni taypi number bolmasa error throw qilsin
-    else if (a <=20 ) callback(null,list[0]);
-    else if (a > 20 && a <= 30) callback(null,list[1]);
-    else if (a > 30 && a <= 40) callback(null,list[1]);
-    else if (a > 40 && a <= 50) callback(null,list[1]);
-    else if (a > 50 && a <= 60) callback(null,list[1]);
-    else {
+// function maslahatBering(a,callback) {
+//     if (typeof a !== 'number') callback("insert a number",null); //agar kiritilgan a argumentni taypi number bolmasa error throw qilsin
+//     else if (a <=20 ) callback(null,list[0]);
+//     else if (a > 20 && a <= 30) callback(null,list[1]);
+//     else if (a > 30 && a <= 40) callback(null,list[1]);
+//     else if (a > 40 && a <= 50) callback(null,list[1]);
+//     else if (a > 50 && a <= 60) callback(null,list[1]);
+//     else {
        
-        setInterval(function()  {  //resolve = return 
-            callback(list[5]);  // Resolves the promise with the value from list[5] after 5 seconds
-         }, 1000);
+//         setInterval(function()  {  //resolve = return 
+//             callback(list[5]);  // Resolves the promise with the value from list[5] after 5 seconds
+//          }, 1000);
                    
     
 
-    }
-}
+//     }
+// }
 
-console.log('passed here 1')
-   maslahatBering(70,(err,data) => { 
-       if(err) console.log("Error",err);
-       else {
-           console.log('javob',data);
-       };
-   });
-   console.log('passed here 2');
+// console.log('passed here 1')
+//    maslahatBering(70,(err,data) => { 
+//        if(err) console.log("Error",err);
+//        else {
+//            console.log('javob',data);
+//        };
+//    });
+//    console.log('passed here 2');
 
 
