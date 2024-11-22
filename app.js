4dc28afd0777f1643b1537f3033d0e072f9c234f
ADAM <shavgoniaziz@gmail.com>
2024-11-22 18:08:23 +0500

    feat: MongoDB connection logic

---
+++ app.js
@@ -0,0 +1,84 @@
+console.log("Web serverni boshlash");
+const express = require('express');
+const app = express(); // express instance yani bu orqali biz express objectni ishlatamiz
+const http = require('http'); // http bu bizning core modulimiz
+const fs = require('fs');
+
+let user;
+//Fake Data Base Server
+fs.readFile("database/user.json","utf8",(err,data)=> {
+    if (err) {
+        console.log("ERROR")
+    }else {
+        user = JSON.parse(data);
+    }
+});
+
+//MongoDB choqirish
+const db = require("./server").db(); //MongoDB objectni yani instance qurib beryapti va biz bu orqali biz Database ga turli xil malumotlarni yozish o'chirish ishlarini qilishimiz mumkin yani crud operation qila olamiz
+
+
+
+// 1 **KIRISH** expressga kirib kelgan codelar yoziladi
+app.use(express.static("public")); // har qanday browserdan kirib kelayotgan zaproslar uchun public folder ochiq degani yani faqat public folderni clientlarga ochib beryapmiz
+app.use(express.json()); // kirib kelyotgan json formatdagi datani obj holatga o'girib beradi, bu bizga kirib kelayotgan object formatdagi datani json formatga otkazib beradi
+app.use(express.urlencoded({extended: true})); // buni yozmasak html formdan post qilingan malumotlarni express serverimiz ignore qiladi yani serverga kiritmaydi; HTML form post shaklida kevotgan requestlar qabul qivolamiza
+
+// 2 **Session CODE** 
+// (Bu yerda Session code yozilishi mumkin)
+
+// 3 **VIEWS CODE** Backenda front end yasaymiz (VIEW yasaymiz)
+// BSSR back-end-server-side renderring yo'nalishini tanladik
+app.set("views", "views"); // yani views folderni ichidan o'qiydi
+app.set("view engine", "ejs"); // view engine ejs ligini ko'rsatib beryabmiz, ejs orqali back endni ichida front end yasaymiz
+
+// 4 **Router'larga** mo'ljallangan
+/* 
+app.get("/hello", function(req, res){
+    res.end(`<h1 style="background: orange">HELLO MIT</h1>`);
+});
+
+app.get("/gift", function(req, res){
+    res.end(`<h1 style="background: green">GIFT PAGE</h1>`);
+});
+*/
+/*
+get bizga malumotni olihs uchun get ishlatilinadi 
+post esa ozi bilan malum bir malumotni olib keladi va  databasega yozadi
+
+HTTP 3 qismdan iborat (starter line) (headers) (request body =biz yozgan malumotni u body sectionga olib qo'yadi)
+
+*/
+
+
+
+
+app.post('/create-item' , (req,res) => {
+ console.log(req.body);
+ res.json({test:"success"})
+});
+
+app.get('/author', (req,res) => {
+    res.render('author', { user:user});
+})
+
+app.get('/',function(req, res) {
+    res.render('reja');
+})
+
+
+
+// git reset --hard oxirgi versidagi push qilingan codega olib boradi
+// express orqali web server qurib oldik.
+// front end'ni qurish 2 usuli bor: 1) Backend'da front qurib olish BSSR using EJS 2) React - single page rendering
+
+// git clean -df oxirgi ruchnoy kiritgan narsalarni udalit qiladi
+// git pull origin master orqali eng oxirgi source ni yuklab olamiz
+// git ni bitta orqaga qaytarish uchun
+// git reset --hard desak eng oxirgi deploy qilingan holga qaytaradi kodni
+// get bizga malumotni data base dan olish uchun ishlatilinadi 
+// post esa malumotni ozi bilan olib keladi va data base ga malum bir malumotni yozadi
+
+// secure socket layer bolmagani uchun ham bizda localhost link bilan kirmadi 
+
+module.exports = app; //app ni export qilyapmiz
--- server.js
+++ server.js
@@ -1,80 +1,28 @@
-console.log("Web serverni boshlash");
-const express = require('express');
-const app = express(); // express instance yani bu orqali biz express objectni ishlatamiz
 const http = require('http'); // http bu bizning core modulimiz
-const fs = require('fs');
+//const app = require ('./app') //app.js da export qilgan modulimizni ozimizga chaqiryapmiz | pastga ko'chirdik databasega errorsiz ulanganan keyn chaqirilishi uchun
+const mongodb = require('mongodb');
+let db;
+//mongoDB connect
+//MOngoDBga ulanib keyin serverni ishga tushirish kerak
+const connectionString = 
+"mongodb+srv://shavgoniaziz:Aminboy7007$@cluster0.a1vwj.mongodb.net/";
 
-let user;
-fs.readFile("database/user.json","utf8",(err,data)=> {
-    if (err) {
-        console.log("ERROR")
-    }else {
-        user = JSON.parse(data);
-    }
-});
-// 1 **KIRISH** expressga kirib kelgan codelar yoziladi
-app.use(express.static("public")); // har qanday browserdan kirib kelayotgan zaproslar uchun public folder ochiq degani yani faqat public folderni clientlarga ochib beryapmiz
-app.use(express.json()); // kirib kelyotgan json formatdagi datani obj holatga o'girib beradi, bu bizga kirib kelayotgan object formatdagi datani json formatga otkazib beradi
-app.use(express.urlencoded({extended: true})); // buni yozmasak html formdan post qilingan malumotlarni express serverimiz ignore qiladi yani serverga kiritmaydi; HTML form post shaklida kevotgan requestlar qabul qivolamiza
+mongodb.connect(connectionString,
+               {useNewUrlParser: true, 
+                useUnifiedTopology: true,
 
-// 2 **Session CODE** 
-// (Bu yerda Session code yozilishi mumkin)
-
-// 3 **VIEWS CODE** Backenda front end yasaymiz (VIEW yasaymiz)
-// BSSR back-end-server-side renderring yo'nalishini tanladik
-app.set("views", "views"); // yani views folderni ichidan o'qiydi
-app.set("view engine", "ejs"); // view engine ejs ligini ko'rsatib beryabmiz, ejs orqali back endni ichida front end yasaymiz
-
-// 4 **Router'larga** mo'ljallangan
-/* 
-app.get("/hello", function(req, res){
-    res.end(`<h1 style="background: orange">HELLO MIT</h1>`);
-});
-
-app.get("/gift", function(req, res){
-    res.end(`<h1 style="background: green">GIFT PAGE</h1>`);
-});
-*/
-/*
-get bizga malumotni olihs uchun get ishlatilinadi 
-post esa ozi bilan malum bir malumotni olib keladi va  databasega yozadi
-
-HTTP 3 qismdan iborat (starter line) (headers) (request body =biz yozgan malumotni u body sectionga olib qo'yadi)
-
-*/
-
-
-
-
-app.post('/create-item' , (req,res) => {
- console.log(req.body);
- res.json({test:"success"})
-});
-
-app.get('/author', (req,res) => {
-    res.render('author', { user:user});
-})
-
-app.get('/',function(req, res) {
-    res.render('reja');
-})
-
-// serverni yaratamiz va unga app ni yuklaymiz
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function() {
-    console.log(`The server is running successfully on port: ${PORT},  https://localhost:${PORT}`);
-});
-
-// git reset --hard oxirgi versidagi push qilingan codega olib boradi
-// express orqali web server qurib oldik.
-// front end'ni qurish 2 usuli bor: 1) Backend'da front qurib olish BSSR using EJS 2) React - single page rendering
-
-// git clean -df oxirgi ruchnoy kiritgan narsalarni udalit qiladi
-// git pull origin master orqali eng oxirgi source ni yuklab olamiz
-// git ni bitta orqaga qaytarish uchun
-// git reset --hard desak eng oxirgi deploy qilingan holga qaytaradi kodni
-// get bizga malumotni data base dan olish uchun ishlatilinadi 
-// post esa malumotni ozi bilan olib keladi va data base ga malum bir malumotni yozadi
-
-// secure socket layer bolmagani uchun ham bizda localhost link bilan kirmadi 
+    }, (err,client) => {  //MongoDbga ulansa mongodb ni cleintini yani instancini olib beradi yani error mavjud bolmasa pass qiladi.
+      if(err) console.log("Error: ON MongoDb connection" );
+      else {
+        console.log("Connected to MongoDB successfully!");
+        //console.log(client); //Mongodb clientni korsa boladi 
+        module.exports = client;// kelajakda clientni kop ishlatamiz shuning uchun biz clientni server.js fayldan export qilib olishimiz kerak. cleintni ichida db degan object bor shuning uchun ham clientni export qilib olamiz 
+        const app = require ('./app') //app.js da export qilgan modulimizni ozimizga chaqiryapmiz va shu run bolyapti 
+        // serverni yaratamiz va unga app ni yuklaymiz
+        const server = http.createServer(app); // tuzga app.js orqali express srverni ishga tushuryapmiz
+        let PORT = 3000;
+        server.listen(PORT, function() { //va 3000 chi portda listen bolyapti 
+            console.log(`The server is running successfully on port: ${PORT},  https://localhost:${PORT}`);
+        });    
+      }
+    });//ikkita narsani true holatda pass qilish kerak 
